690229b325683692575783f821924df219388d0b
Alexander <Snakeage@gmail.com>
2024-07-31 10:46:59 +0300

    adding cart context and shopping icon as well as cart dropdown

---
+++ src/contexts/cart-context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from 'react';
+
+import CartDropdown from '../components/cart-dropdown/cart-dropdown.component';
+
+export const CartContext = createContext({
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+});
+
+export const CartProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const value = { isCartOpen, setIsCartOpen };
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
