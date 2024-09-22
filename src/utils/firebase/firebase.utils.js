4185c0b85a528544072d89dd3ce378ef41edbc35
Alexander <Snakeage@gmail.com>
2024-09-22 16:15:23 +0300

    Deleted const firebaseApp = from firebase.utils.js, because it makes a bloblem with deploying in netlify

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,7 +29,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
